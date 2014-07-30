3a735e94be12692ecfae78ef4f9cc11e367d1108
Jeff VanOss <jeff.vanoss@dornerworks.com>
2014-07-30 11:59:50 -0400

    removed dev tests from boggle.py and formalized them in boggleTest.py

--- boggle.py
+++ boggle.py
@@ -1,55 +1,56 @@
 import random
 import pickle
-import timeit
-import multiprocessing as mp 
+import multiprocessing as mp
+
 
 class Board:
-    def __init__(self, shape=(4,4), board=None):
-        self.diceVal=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','qu','r','s','t','u','v','w','x','y','z']
-        self.maxWordLen=15 #28 for wordsEn.txt
-        self.minWordLen=3
+    def __init__(self, shape=(4, 4), board=None):
+        self.diceVal = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+                        'l', 'm', 'n', 'o', 'p', 'qu', 'r', 's', 't', 'u', 'v',
+                        'w', 'x', 'y', 'z']
+        self.maxWordLen = 15  # 28 for wordsEn.txt
+        self.minWordLen = 3
         self.shape = shape
 #         self.board = [[ random.choice(self.diceVal) for _y in range(shape[1])] for _x in range(shape[0])]
-        
+
         self.board = {}
-        if board is None or len(board) < shape[0]*shape[1]:
+        if board is None or len(board) < shape[0] * shape[1]:
             for x in range(self.shape[0]):
                 for y in range(self.shape[1]):
-                    self.board[(x,y)] = random.choice(self.diceVal)
-        else: # load from board
-            indx=0
+                    self.board[(x, y)] = random.choice(self.diceVal)
+        else:  # load from board
+            indx = 0
             for x in range(self.shape[0]):
                 for y in range(self.shape[1]):
-                    self.board[(x,y)] = board[indx]
-                    indx+=1
-        
-        
+                    self.board[(x, y)] = board[indx]
+                    indx += 1
+
         self.wordhash = {}
         try:
             self.wordhash = self._loadWordHash()
         except:
             self.wordhash = self._makeWordHash(save=True)
-        
+
     def __str__(self):
-        s = '|' + '-'*(3*self.shape[1]) + '|\n'
+        s = '|' + '-' * (3 * self.shape[1]) + '|\n'
         for x in range(self.shape[0]):
             s += '|'
             for y in range(self.shape[1]):
-                s += '{:3}'.format(self.board[(x,y)])
+                s += '{:3}'.format(self.board[(x, y)])
             s += '|\n'
-        s += '|' + '-'*(3*self.shape[1]) + '|\n'
+        s += '|' + '-' * (3 * self.shape[1]) + '|\n'
         return s
 
     def _makeWordHash(self, save=False):
         with open('TWL06.txt', 'r') as f:
             words = f.readlines()
-            
+
         for indx in range(len(words)):
-            words[indx]=words[indx].strip().lower()
-        
+            words[indx] = words[indx].strip().lower()
+
         wordHash = {}
         for word in words:
-            for indx in range(1,len(word)+1):
+            for indx in range(1, len(word) + 1):
                 if word[:indx] not in wordHash:
                     wordHash[word[:indx]] = False
                 if indx == len(word):
@@ -58,7 +59,7 @@ class Board:
             with open("wordHash.pickle", 'wb') as f:
                 pickle.dump(wordHash, f)
         return wordHash
-    
+
     def _loadWordHash(self):
         with open("wordHash.pickle", 'rb') as f:
             wordHash = pickle.load(f)
@@ -68,83 +69,60 @@ class Board:
         words = set()
         for x in range(self.shape[0]):
             for y in range(self.shape[1]):
-                words.update(self._findWordsRec(self.board[(x,y)], [(x,y)]))
+                words.update(self._findWordsRec(self.board[(x, y)], [(x, y)]))
         return words
-    
+
     def findWordsMP(self):
         words = set()
 
-        inputs=[]
+        inputs = []
         for x in range(self.shape[0]):
             for y in range(self.shape[1]):
-                inputs.append((self.board[(x,y)], [(x,y)]))
-                
+                inputs.append((self.board[(x, y)], [(x, y)]))
+
         pool_size = mp.cpu_count()
         if pool_size > 1:
-            pool_size -= 1 #leave one core for other tasks
+            pool_size -= 1  # leave one core for other tasks
         pool = mp.Pool(processes=pool_size)
         pool_outputs = pool.map(self._findWordsRecMP, inputs)
         pool.close()
         pool.join()
-        
+
         for output in pool_outputs:
             words.update(output)
-            
+
         return words
-    
+
     def _findWordsRecMP(self, args):
         return self._findWordsRec(*args)
-    
+
     '''
-    returns set of words possible given starting coordinates    
+    returns set of words possible given starting coordinates
     '''
     def _findWordsRec(self, word, points):
         words = set()
         if len(points) < self.maxWordLen:
             for point in self._addPoint(points):
-                newWord = word+self.board[point]
+                newWord = word + self.board[point]
                 if newWord in self.wordhash:
-                    newPoints = list(points); newPoints.append(point)
+                    newPoints = list(points) + [point]
                     words.update(self._findWordsRec(newWord, newPoints))
                     if len(newWord) >= self.minWordLen and self.wordhash[newWord]:
                         words.add(newWord)
         return words
-    
+
     def _addPoint(self, points):
         x, y = points[-1]
-        for p in [(x-1, y-1), 
-                  (x-1, y  ), 
-                  (x-1, y+1), 
-                  (x  , y-1), 
-                  (x  , y+1), 
-                  (x+1, y-1), 
-                  (x+1, y  ), 
-                  (x+1, y+1)]:
-            if 0<=p[0]<self.shape[0] and 0<=p[1]<self.shape[1] and p not in points:
+        for p in [(x - 1, y - 1),
+                  (x - 1, y),
+                  (x - 1, y + 1),
+                  (x, y - 1),
+                  (x, y + 1),
+                  (x + 1, y - 1),
+                  (x + 1, y),
+                  (x + 1, y + 1)]:
+            if 0 <= p[0] < self.shape[0] and 0 <= p[1] < self.shape[1] and p not in points:
                 yield p
 
 if __name__ == '__main__':
-#     game = Board((10,10))
-#     print(game)
-#     words = game.findWords()
-#     print(words)
-#     words = game.findWordsMP()
-#     print(words)
-#     print(len(words))
-#     print("findWords():   ", timeit.timeit("words=game.findWords()", "from __main__ import game, words", number=10)/10.0)
-#     print("findWordsMP(): ", timeit.timeit("words=game.findWordsMP()", "from __main__ import game, words", number=4)/4.0)
-    
-#     import cProfile
-#     cProfile.run("game.findWords()")
-    
-    game = Board((200,200))
-    print(game)
-    words = game.findWords()
-    print(words)
-    print(len(words))
-    print("findWords():   ", timeit.timeit("words=game.findWords()", "from __main__ import game, words", number=1))
-    print("findWordsMP(): ", timeit.timeit("words=game.findWordsMP()", "from __main__ import game, words", number=1))
-# 
-#     import cProfile
-#     cProfile.run("game.findWords()")
-    
+    pass
--- boggleTest.py
+++ boggleTest.py
@@ -4,35 +4,55 @@ Created on Jul 29, 2014
 @author: jeffvanoss
 '''
 import unittest
+import timeit
 import boggle
 
-class Test(unittest.TestCase):
 
+class AccuracyTests(unittest.TestCase):
 
     def setUp(self):
         pass
 
+    def tearDown(self):
+        pass
+
+    def test1(self):
+        answer = ['chield', 'etched', 'miched', 'pithed', 'teched', 'techie', 'aahed', 'ached', 'acted', 'amice', 'cheth', 'chiel', 'cited', 'deice', 'diced', 'eched', 'ethic', 'lethe', 'maced', 'mache', 'miche', 'theca', 'aced', 'ache', 'acid', 'ahed', 'amah', 'amid', 'amie', 'cede', 'cham', 'chid', 'chip', 'chit', 'cite', 'dice', 'died', 'diel', 'diet', 'dipt', 'dite', 'eche', 'echt', 'elhi', 'etch', 'etic', 'haha', 'held', 'heth', 'hied', 'iced', 'mace', 'mach', 'mica', 'mice', 'mite', 'pica', 'pice', 'pied', 'pima', 'pith', 'tech', 'tied', 'aah', 'ace', 'act', 'aha', 'ahi', 'ama', 'ami', 'cam', 'chi', 'del', 'die', 'dim', 'dip', 'dit', 'edh', 'eld', 'eth', 'hah', 'ham', 'heh', 'het', 'hic', 'hid', 'hie', 'him', 'hip', 'hit', 'ice', 'ich', 'led', 'lei', 'let', 'mac', 'mic', 'mid', 'pic', 'pie', 'pit', 'ted', 'tel', 'the', 'tic', 'tie', 'tip']
+
+        b = boggle.Board((4, 4), ['p', 't', 'e', 'l', 'd', 'i', 'h', 'd', 'h', 'm', 'c', 'e', 'a', 'a', 'h', 't'])
+        words = b.findWords()
+
+        self.assertCountEqual(answer, words)
+
+    def test2(self):
+        answer = ['ostomy', 'stomal', 'atomy', 'gogos', 'slats', 'slaty', 'stoma', 'stows', 'swots', 'atom', 'glam', 'gogo', 'lats', 'mats', 'mogs', 'most', 'mots', 'slam', 'slat', 'soma', 'sows', 'stow', 'swot', 'togs', 'tost', 'tows', 'wogs', 'wost', 'wots', 'als', 'gos', 'got', 'goy', 'lam', 'lat', 'lax', 'mat', 'max', 'mog', 'mos', 'mot', 'som', 'sot', 'sow', 'soy', 'sty', 'tam', 'tax', 'tog', 'tom', 'tow', 'toy', 'wog', 'wos', 'wot', 'yom', 'zax']
+
+        b = boggle.Board((4, 4), ['y', 'o', 's', 't', 'm', 't', 'g', 'o', 'a', 'x', 'g', 'w', 'z', 'l', 's', 'm'])
+        words = b.findWords()
+
+        self.assertCountEqual(answer, words)
+
+
+class SpeedTests(unittest.TestCase):
+
+    def setUp(self):
+        pass
 
     def tearDown(self):
         pass
 
+    def test10x10Serial(self):
+        print("10x10   findWords():   ", timeit.timeit("game.findWords()", "import boggle; game=boggle.Board((10, 10))", number=10) / 10.0)
 
-    def test1(self):
-        answer = ['chield', 'etched', 'miched', 'pithed', 'teched', 'techie', 'aahed', 'ached', 'acted', 'amice', 'cheth', 'chiel', 'cited', 'deice', 'diced', 'eched', 'ethic','lethe', 'maced', 'mache', 'miche', 'theca', 'aced', 'ache', 'acid', 'ahed', 'amah', 'amid', 'amie', 'cede', 'cham', 'chid', 'chip', 'chit', 'cite', 'dice', 'died', 'diel', 'diet', 'dipt', 'dite', 'eche', 'echt', 'elhi', 'etch', 'etic', 'haha', 'held', 'heth', 'hied', 'iced', 'mace', 'mach', 'mica', 'mice', 'mite', 'pica', 'pice', 'pied', 'pima', 'pith', 'tech', 'tied', 'aah', 'ace', 'act', 'aha','ahi', 'ama', 'ami', 'cam', 'chi', 'del', 'die', 'dim', 'dip', 'dit', 'edh', 'eld', 'eth', 'hah', 'ham', 'heh', 'het', 'hic', 'hid', 'hie', 'him', 'hip', 'hit', 'ice', 'ich', 'led', 'lei', 'let', 'mac', 'mic', 'mid', 'pic', 'pie', 'pit', 'ted', 'tel', 'the', 'tic', 'tie', 'tip']
-        
-        b = boggle.Board((4,4), ['p', 't', 'e', 'l', 'd', 'i', 'h', 'd', 'h', 'm', 'c', 'e', 'a', 'a', 'h', 't'])
-        words = b.findWords()
-        
-        self.assertCountEqual(answer, words)
+    def test10x10Parallel(self):
+        print("10x10   findWordsMP(): ", timeit.timeit("game.findWordsMP()", "import boggle; game=boggle.Board((10, 10))", number=4) / 4.0)
 
-    def test2(self):
-        answer = ['ostomy', 'stomal', 'atomy', 'gogos', 'slats', 'slaty', 'stoma', 'stows', 'swots', 'atom', 'glam', 'gogo', 'lats', 'mats', 'mogs', 'most', 'mots', 'slam', 'slat', 'soma', 'sows', 'stow', 'swot', 'togs', 'tost', 'tows', 'wogs', 'wost', 'wots', 'als', 'gos', 'got', 'goy', 'lam', 'lat', 'lax', 'mat', 'max', 'mog', 'mos','mot', 'som', 'sot', 'sow', 'soy', 'sty', 'tam', 'tax', 'tog', 'tom', 'tow', 'toy', 'wog', 'wos', 'wot', 'yom', 'zax']
+    def test100x100Serial(self):
+        print("100x100 findWords():   ", timeit.timeit("game.findWords()", "import boggle; game=boggle.Board((100, 100))", number=1))
 
-        b = boggle.Board((4,4), ['y', 'o', 's', 't', 'm', 't', 'g', 'o', 'a', 'x', 'g', 'w', 'z', 'l', 's', 'm'])
-        words = b.findWords()
-        
-        self.assertCountEqual(answer, words)
+    def test100x100Parallel(self):
+        print("100x100 findWordsMP(): ", timeit.timeit("game.findWordsMP()", "import boggle; game=boggle.Board((100, 100))", number=1))
 
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
+    # import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
